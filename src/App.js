0b7cbae1ed64c8c0e9544a4b60af52121395fc46
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-03 22:20:56 -0700

    Search functional and results set to state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.scss';
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
 import SearchResults from './components/SearchResults/SearchResults';
 import Nominations from './components/Nominations/Nominations';
 
+const baseURL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+
 function App() {
+
+  const [ results, setResults ] = useState('');
+  const [ nominations, setNominations ] = useState('');
+  const [ resultsPageNum, setResultsPageNum ] = useState(1);
+  
   return (
     <>
       <Header />
-      <Search />
-      <SearchResults />
-      <Nominations />
+      <Search 
+        setResults={ setResults }
+        baseURL={ baseURL }
+        resultsPageNum={ resultsPageNum }
+      />
+      <SearchResults
+        results={ results }
+        setNominations={ setNominations }
+        setResultsPageNum={ setResultsPageNum }
+      />
+      <Nominations nominations={ nominations }/>
     </>
   );
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
-function Search() {
+function Search({ setResults, baseURL, resultsPageNum }) {
+
+  const [ searchTerm, setSearchTerm ] = useState('');
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      Axios
+      .get(`${baseURL}&s=${searchTerm}&type=movie&page=${resultsPageNum}`)
+      .then((res) => {
+        setResults(res.data);
+      })
+      .catch(err => console.log(err))
+    }
+  }, [searchTerm, baseURL, resultsPageNum, setResults])
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (searchTerm !== '') {
+      Axios
+      .get(`${baseURL}&s=${searchTerm}&type=movie&page=${resultsPageNum}`)
+      .then((res) => {
+        setResults(res.data);
+      })
+      .catch(err => console.log(err))
+    }
+  }
+
   return (
     <div>
-      Search
+      <form onSubmit={submitHandler}>
+        <div>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            onChange={handleInputChange}
+            value={searchTerm}
+            placeholder="Search movies"
+          />
+        </div>
+        <div>
+          <button type="submit">Search</button>
+        </div>
+      </form>
     </div>
   )
 }
