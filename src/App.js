0b922293a6dc960b51e3ddd9524e3f0b67700c81
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-03 17:42:48 -0700

    Setup directories for various components, added axios and sass to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Search from './components/Search/Search';
+import SearchResults from './components/SearchResults/SearchResults';
+import Nominations from './components/Nominations/Nominations';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Search />
+      <SearchResults />
+      <Nominations />
+    </>
   );
 }
 
---
+++ src/components/Nominations/Nominations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nominations() {
+  return (
+    <div>
+      Nominations
+    </div>
+  )
+}
+
+export default Nominations
