140f3fd3412fe2c766661dd0540735a28be1f4cb
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-04 13:21:31 -0700

    Nominations now visible on page

--- src/components/Nominations/Nominations.jsx
+++ src/components/Nominations/Nominations.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
-function Nominations() {
-  return (
-    <div>
-      Nominations
-    </div>
-  )
+function Nominations({ nominations, setNominations }) {
+  
+  const handleRemoveNomination = (movieID) => {
+    console.log(movieID)
+    // let movie = { imdbID, Title, Year, Poster };
+    // setNominations([...nominations, movie]);
+  }
+
+  if(nominations){
+    return (
+      <section>
+        <ul>
+          {
+            nominations.map(movie => {
+              return (
+                <li key={movie.imdbID}>
+                  <div>
+                    <img src={movie.Poster} alt=""/>
+                  </div>
+                  <div>
+                    <h3>{movie.Title}</h3>
+                    <p>Year: {movie.Year}</p>
+                    <button onClick={() => handleRemoveNomination(movie.imdbID)}>Remove!</button>
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
+    )
+  } else return (<></>)
 }
 
 export default Nominations
