224b900029b429cd7632e22169e1e637b203fdf6
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-05 13:32:05 -0700

    Styled nominations component

--- src/components/Nominations/Nominations.jsx
+++ src/components/Nominations/Nominations.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {v4 as uuid} from 'uuid';
 import './Nominations.scss';
+import DeleteIcon from '../../assets/icons/delete.svg';
 
 function Nominations({ nominations, setNominations, nominationsVisibility }) {
   
@@ -8,34 +9,46 @@ function Nominations({ nominations, setNominations, nominationsVisibility }) {
     setNominations(nominations.filter(movie => movie.imdbID!==movieID));
   }
 
-  const isNominationsVisible = nominationsVisibility ? "visible" : "hidden";
+  const isNominationsVisible = nominationsVisibility ? "nominations nominations--visible" : "nominations--hidden";
 
   if(nominations.length!==0){
     return (
       <section className={isNominationsVisible}>
-        <ul>
-          {
-            nominations.map(movie => {
-              return (
-                <li key={uuid()}>
-                  <div>
-                    <img src={movie.Poster} alt=""/>
-                  </div>
-                  <div>
-                    <h3>{movie.Title}</h3>
-                    <p>Year: {movie.Year}</p>
-                    <button onClick={() => handleRemoveNomination(movie.imdbID)}>Remove!</button>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
+        <div className="nominations__block">
+          <h3 className="nominations__block-title">Your Nominations</h3>
+          <ul className="nominations__block-list">
+            {
+              nominations.map(movie => {
+                return (
+                  <li className="nominations__block-list-item" key={uuid()}>
+                    <div className="nominations__block-list-item-poster">
+                      <img className="nominations__block-list-item-poster-img" src={movie.Poster} alt=""/>
+                    </div>
+                    <div className="nominations__block-list-item-desc">
+                      <h3 className="nominations__block-list-item-desc-title">{movie.Title}</h3>
+                      <p className="nominations__block-list-item-desc-year">{movie.Year}</p>
+                      <button
+                        className="nominations__block-list-item-desc-button"
+                        onClick={() => handleRemoveNomination(movie.imdbID)}>
+                        <img
+                          className="nominations__block-list-item-desc-button-icon"
+                          src={DeleteIcon}
+                          alt="Remove"/>
+                      </button>
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
       </section>
     )
   } else return (
     <section className={isNominationsVisible}>
-      <h3>Nominate some movies to see them here!</h3>
+      <div className="nominations__block">
+        <h3 className="nominations__block-title">Nominate some movies to see them here!</h3>
+      </div>
     </section>
   )
 }
