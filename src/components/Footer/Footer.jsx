a86a06a2936d7ddaa2538a952f483986e9e41dd9
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-07 20:52:30 -0700

    Fix styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,18 +5,18 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__block container">
-        <p className="footer__block-credits">
+        <div className="footer__block-credits">
           <h4 className="footer__block-credits-heading">Website by:</h4>
           <a className="footer__block-credits-link" href="https://jaspreetlall.com">Jaspreet Lall</a>
-        </p>
-        <p className="footer__block-credits">
+        </div>
+        <div className="footer__block-credits">
           <h4 className="footer__block-credits-heading">OMDb API by:</h4>
           <a className="footer__block-credits-link" href="https://https://www.omdbapi.com/">Brian Fritz</a>
-        </p>
-        <p className="footer__block-credits">
+        </div>
+        <div className="footer__block-credits">
           <h4 className="footer__block-credits-heading">Cover photo by:</h4>
           <a className="footer__block-credits-link" href="https://unsplash.com/@mbuff">Sung Jin Cho</a>
-        </p>
+        </div>
       </div>
     </footer>
   )
